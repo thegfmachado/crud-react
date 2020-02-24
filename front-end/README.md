7875805934c838eac2d98a88bfec0b1c972e84dd
1GabrielMachado1 <gabriel.ferreira.machado@hotmail.com>
2020-02-23 23:13:57 -0300

    Commit do projeto já finalizado.

---
+++ front-end/README.md
@@ -0,0 +1,2 @@
+# user-crud-react
+Projeto para fixação de conhecimento o qual foi desenvolvido crud simples de usuários utilizando React JS.
---
+++ front-end/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início" subtitle="Primeiro projeto CRUD utilizando React!">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema p/ exemplificar um CRUD desenvolvido em React</p>
+    </Main>
